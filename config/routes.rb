eec63cbd44d658925692e119ccc23df2f69213a8
leovanel <leovanel>
2020-01-07 15:42:00 +0100

    namespace admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
- 
+  namespace :admin do
+    resources :users
+    resources :rubrics 
+    resources :articles
+  end
+
   devise_for :users
 
   resources :rubrics 

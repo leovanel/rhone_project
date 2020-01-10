501482ea17c7c82cb335c3118cea727231fb1ab5
leovanel <leovanel>
2020-01-10 16:12:32 +0100

    def create articles admin

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -16,6 +16,16 @@ class Admin::ArticlesController < ApplicationController
 
   def create
 
+    @article = Article.new(title: params[:title],body: params[:body],user_id : params[:user_id],rubric_id: rubrics_params[:id])
+    if @article.save
+      flash[:success] = 'Rubric successfully created'
+      redirect_to (admin_article_path(@article.id))
+       
+    else
+      flash.now[:danger] = 'Something went wrong, please check your input'
+      render new_admin_rubric_path
+    end
+
   end
 
   def edit
@@ -28,6 +38,13 @@ class Admin::ArticlesController < ApplicationController
 
   def destroy
 
+  end
+
+  private  
+
+  def rubrics_params
+
+    params.require(:rubric).permit(:id)
   end
 
 end

2a5e29b12f533b878767da24f013992d0ad2e016
leovanel <leovanel>
2020-01-16 10:42:35 +0100

    relations paragraph

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
 
   has_many_attached :article_images
+  has_many :paragraphs
 
   belongs_to :rubric
   belongs_to :user
--- app/models/paragraph.rb
+++ app/models/paragraph.rb
@@ -1,3 +1,4 @@
 class Paragraph < ApplicationRecord
   belongs_to :article
+  has_one_attached :ph_image
 end
